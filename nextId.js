f6c273be6c6a98a92c4db6211a3683fb6491ded3
ed system on behalf of Gizelle van Zyl <ed>
2021-06-03 23:45:58 +0000

    Web submission

--- nextId.js
+++ nextId.js
@@ -2,11 +2,11 @@ function nextId(data) {
     // your code here
     //Check that there is a data array and that it is not empty i.e. it has a length
     if (Array.isArray(data) && data.length) {    
-        //get highest value in array
-       return (Math.max([data.id]) + 1)
+        //sort ascending
+        let sortedData = [data.id].sort(function(a, b){return b-a})
         
-        //let lastDataEntry = data.length - 1]
-        //return lastDataEntry.id + 1
+        let lastDataEntry = sortedData[sortedData.length - 1]
+        return lastDataEntry.id + 1
 
     //if there is no data array or if it is empty, return 1
     } else {
