e8c3cad720d2da6deb4ee0e98f171f54770d1110
ed system on behalf of Gizelle van Zyl <ed>
2021-06-03 23:39:50 +0000

    Web submission

--- nextId.js
+++ nextId.js
@@ -3,7 +3,7 @@ function nextId(data) {
     //Check that there is a data array and that it is not empty i.e. it has a length
     if (Array.isArray(data) && data.length) {    
         //return highest id in array + 1
-        return data.id.sort(function(a, b){return b-a})] + 1
+        return data.id.sort(function(a, b){return b-a}) + 1
     //if there is no data array or if it is empty, return 1
     } else {
         return 1
