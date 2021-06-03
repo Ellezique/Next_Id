905d1279992d4166290b64091de793ae87a6224b
ed system on behalf of Gizelle van Zyl <ed>
2021-06-03 23:31:25 +0000

    Web submission

--- nextId.js
+++ nextId.js
@@ -3,8 +3,8 @@ function nextId(data) {
     //Check that there is a data array and that it is not empty i.e. it has a length
     if (Array.isArray(data) && data.length) {
         //if there is data, sort the data array in ascending order
-        data.sort(function(a, b){return a-b})
-        let lastDataEntry = data[data.length - 1]
+        let sortedData = data.sort(function(a, b){return a-b})
+        let lastDataEntry = sortedData[sortedData.length - 1]
         return lastDataEntry.id + 1
     //if there is no data array or if it is empty, return 1
     } else {
