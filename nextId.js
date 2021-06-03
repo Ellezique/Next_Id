4bd03ce99b51b085be5972666258909826714357
ed system on behalf of Gizelle van Zyl <ed>
2021-06-03 23:43:18 +0000

    Web submission

--- nextId.js
+++ nextId.js
@@ -3,7 +3,7 @@ function nextId(data) {
     //Check that there is a data array and that it is not empty i.e. it has a length
     if (Array.isArray(data) && data.length) {    
         //get highest value in array
-       return Math.max(data.id) + 1
+       return (Math.max(data.id) + 1)
         
         //let lastDataEntry = data.length - 1]
         //return lastDataEntry.id + 1
