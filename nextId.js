220ce490f24f101a57f24e74e3963161d470da94
ed system on behalf of Gizelle van Zyl <ed>
2021-06-03 23:28:20 +0000

    Web submission

--- nextId.js
+++ nextId.js
@@ -1,24 +1,14 @@
 function nextId(data) {
     // your code here
-let lastDataEntry = data[data.length - 1];
-return lastDataEntry.id + 1
+    //Check that there is a data array and that it is not empty i.e. it has a length
+    if (Array.isArray(data) && data.length) {
+        let lastDataEntry = data[data.length - 1]
+        return lastDataEntry.id + 1
+    //if there is no data array or if it is empty, return 1
+    } else {
+        return 1
+    }
 }
 
-let data = [
-   {
-     id: 1,
-     label: "Rose"
-   },
-   {
-     id: 3,
-     label: "Tiger"
-   },
-   {
-     id: 10,
-     label: "Tree"
-   }
-]  
-
-console.log(nextId(data))
 
 module.exports = nextId
